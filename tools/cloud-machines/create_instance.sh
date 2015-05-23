24d0c6491e9ca64e825cf94f5019dada5955bdc2
Tim Head <betatim@gmail.com>
2015-05-23 14:23:17 +0200

    Do not reuse hostnames for instances

--- tools/cloud-machines/create_instance.sh
+++ tools/cloud-machines/create_instance.sh
@@ -2,4 +2,10 @@
 image="8b082f2d-9728-4c3f-8440-cf110f03f745"
 keypair="timslaptop"
 
-openstack server create --key-name "$keypair" --flavor m1.large --image "$image" --user-data "configure-vm.sh" lhcbstarterkit2
+# Make sure to keep cycling these numbers
+# each time we create a new instance and reuse a hostname
+# people will get big fat warnings from ssh as the keys
+# will have changed
+for i in 3 4 5 6 7; do
+    openstack server create --key-name "$keypair" --flavor m1.large --image "$image" --user-data "configure-vm.sh" lhcbstarterkit$i
+done;
