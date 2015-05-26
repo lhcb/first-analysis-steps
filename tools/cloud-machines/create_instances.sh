b060be5c48ad7c0de37a575898d6b93b5edd174f
Tim Head <betatim@gmail.com>
2015-05-26 13:11:34 +0200

    Changed create_instances.sh to use command line args

--- tools/cloud-machines/create_instances.sh
+++ tools/cloud-machines/create_instances.sh
@@ -2,10 +2,9 @@
 image="8b082f2d-9728-4c3f-8440-cf110f03f745"
 keypair="timslaptop"
 
-# Make sure to keep cycling these numbers
-# each time we create a new instance and reuse a hostname
-# people will get big fat warnings from ssh as the keys
-# will have changed
-for i in 8 9 10 11; do
+# Only argument is the list of hostname extensions
+# to use when starting instances. Usually use a number
+# like 2 3 4 5 6 to start five instances.
+for i in $*; do
     openstack server create --key-name "$keypair" --flavor m1.large --image "$image" --user-data "configure-vm.sh" lhcbstarterkit$i
 done;
